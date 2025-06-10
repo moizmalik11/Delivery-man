ed6443e169808fde6ef828e45b90721c3172589d
moizmalik11 <moiz.malik1113@gmail.com>
2025-06-10 06:09:48 +0500

    Mongo set path

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/deliveryman', {
+    const conn = await mongoose.connect('mongodb://127.0.0.1:27017/deliveryman', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
--- server/routes/deliveryRoutes.js
+++ server/routes/deliveryRoutes.js
@@ -9,6 +9,4 @@ router.get('/:id', auth, deliveryController.getDelivery);
 router.put('/:id', auth, deliveryController.updateDelivery);
 router.delete('/:id', auth, deliveryController.deleteDelivery);
 
-
-
 module.exports = router;
