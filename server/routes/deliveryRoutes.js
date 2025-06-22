1e903282a8abf2889a13b6c554936693d2883de6
Moiz Malik <moizmalik11@gmail.com>
2025-06-22 22:06:55 +0500

    Delivery route set

--- server/routes/deliveryRoutes.js
+++ server/routes/deliveryRoutes.js
@@ -6,7 +6,14 @@ const auth = require('../middleware/auth');
 router.get('/', auth, deliveryController.getDeliveries);
 router.post('/', auth, deliveryController.createDelivery);
 router.get('/:id', auth, deliveryController.getDelivery);
+
+
 router.put('/:id', auth, deliveryController.updateDelivery);
 router.delete('/:id', auth, deliveryController.deleteDelivery);
+
+// Socket.io integration
+const { Server } = require('socket.io');
+const io = require('../socket'); // Assuming you have a socket.js file that exports the io instance
+
 
 module.exports = router;
